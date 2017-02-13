dc8a8fc84072dedb5c92f9b29bef794f30d4d54d
Yasin Yusuf <yaasiinyuusuf78@gmail.com>
2017-02-13 20:36:56 +0800

    created two models, one for each user volunteer/mission and linked them to the builtin User model, changed the auth app name cos it is gonna conflict with the builtin django auth app

--- talespace/urls.py
+++ talespace/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from apps.auth.urls import urlpatterns as auth_urls
+from apps.authentication.urls import urlpatterns as auth_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
