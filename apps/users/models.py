a8575e3c6604b6b956562b765ae4555f824855fc
Aaron Myatt <aaronmyatt@gmail.com>
2017-02-20 18:26:25 +0800

    change text to char fields
    changed the following fields:
    company_name
    phone
    school

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,7 @@
+from django.conf import settings
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db import models
-from django.conf import settings
 
 
 class UserDetails(models.Model):
@@ -13,9 +13,9 @@ class UserDetails(models.Model):
 
     user         = models.OneToOneField(to=settings.AUTH_USER_MODEL)
     user_type    = models.CharField(max_length=1, choices=USER_TYPES)
-    company_name = models.TextField()
-    phone        = models.TextField()
-    school       = models.TextField()
+    company_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
+    school = models.CharField(max_length=100)
     description  = models.TextField()
     experience   = models.TextField()
     interests    = models.TextField()
